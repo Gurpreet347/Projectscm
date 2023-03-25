4b3bdcb338af4a3a0e06fb830397d7cd73a357fa
Gurpreet347 <124994059+Gurpreet347@users.noreply.github.com>
2023-03-25 16:07:29 +0530

    Add files via upload

---
+++ scmproject.py
@@ -0,0 +1,167 @@
+print('ULTIMATE CURRENT AFFFAIRS QUIZ')
+a=input("NAME OF THE CONTESTANT : ")
+b=int(input("MOBILE NO. : "))
+c=input("EMAIL ID : ")
+print('By clicking “0” you agree to be bound by Terms and Condition.')
+out = input('type “0” to conti..........\n>>>')
+if out != '0':
+    print('YOU EXITED QUIZ\nTRY AGAIN')
+if(out == '0'):
+    g=0
+    right=g
+    x = 0
+    score = x
+    print("1)Which institution released a report titled ‘Carbon Capture, Utilisation, and Storage Policy Framework?")
+    answer_1 = input("a)ISRO\nb)DRDO\nc)NITI Aayog\nd)Ministry of Environment, Forest and Climate Change\n")
+    if answer_1.lower()== "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("2) Economic Cooperation and Trade Agreement (ECTA) between India and which country is set to enter into force from December 2022?")
+    answer_2 = input("a) UAE\nb) Australia\nc) France\nd) Sri Lanka\n")
+    if answer_2.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("3) Which technology major has announced to set up its first cloud region in Greece?")
+    answer_3 = input("a) Amazon\nb) Microsoft\nc) Google\nd) Meta\n")
+    if answer_3.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("4) ‘CAPSTONE’ which was seen in the news recently, is a satellite launched by which space agency?")
+    answer_4 = input("a) NASA\nb) ESA\nc) SpaceX\nd) Rocosmos\n")
+    if answer_4.lower() == "a":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("5) Which country holds the G20 Presidency in 2022?")
+    answer_5 = input("a)  India\nb) China\nc) Indonesia\nd) Sri Lanka\n")
+    if answer_5.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("6) Which institution/force developed the ‘Drone-based tear smoke launcher’?")
+    answer_6 = input("a) DRDO\nb) BSF\nc) HAL\nd) BEL\n")
+    if answer_6.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("7) Which is the first Indian city to publish the transportation emissions data in collaboration with Google?")
+    answer_7 = input("a)  Pune\nb) Chennai\nc) Aurangabad\nd) Guwahati\n")
+    if answer_7.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("8) ‘Financial Stability Report (FSR)’ is the flagship report released by which institution?")
+    answer_8 = input("a) NITI Aayog\nb) Reserve Bank of India\nc) World Bank\nd) Asian Development Bank\n")
+    if answer_8.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("9) Under ‘India Semiconductor Mission’, financial support is provided for how many years?")
+    answer_9 = input("a) Three\nb) five\nc) six\nd) Ten\n")
+    if answer_9.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("10) Which Union Ministry launched the ‘Services e-Health Assistance and Teleconsultation (SeHAT)’ initiative?")
+    answer_10 = input("a) Defence Ministry\nb) Health and Family Welfare Ministry\nc) Science and Technology Ministry\nd) Electronics and IT Ministry\n")
+    if answer_10.lower() == "a":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("11) Which organisation released a report titled ‘Care at work’?")
+    answer_11 = input("a) World Bank\nb) World Economic Forum\nc) International Labour Organisation\nd) NITI Aayog\n")
+    if answer_11.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("12) Which Indian cricketer has been presented the prestigious ‘Sports Icon’ award?")
+    answer_12 = input("a) Suresh Raina\nb) MS Dhoni\nc) Sachin Tendulkar\nd) Virat Kohli\n")
+    if answer_12.lower() == "a":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("13) Ctrl, Shift and Alt are called .......... keys.")
+    answer_13 = input("a) modifier\nb) function\nc) alphanumeric\nd)  adjustment\n")
+    if answer_13.lower() == "a":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("14) India is not a member of")
+    answer_14 = input("a) G-20\nb) G-8\nc) SAARCc\nd)  U.N\n")
+    if answer_14.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("15) The main aim of SAARC is")
+    answer_15 = input("a) Regional Cooperation\nb) Internal affairs\nc) Non-alignity\nd)  Peaceful Coexistence\n")
+    if answer_15.lower() == "a":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("16) International Maritime Organisation is concerned with")
+    answer_16 = input("a) Air pollution\nb) Sea pollution\nc) Adulteration in food\nd)  Deforestation\n")
+    if answer_16.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("17) How many Indian biosphere reserves are included in the UNESCO World Network of Biosphere Reserves, as of October 2019?")
+    answer_17 = input("a) 14\nb) 11\nc) 17\nd) 9\n")
+    if answer_17.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("18) In the United States the television broadcast standard is...?")
+    answer_18 = input("a) PAL\nb) NTSC\nc) SECAM\nd) RGB\n")
+    if answer_18.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("19) Which consists of two plates separated by a dielectric and can store a charge?")
+    answer_19 = input("a) Inductor\nb) Capacitor\nc) Transistor\nd) Relay\n")
+    if answer_19.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    print("20) What does the term PLC stand for?")
+    answer_20 = input("a) Programmable Lift Computer\nb) Program List Control\nc) Programmable Logic Controller\nd) Piezo Lamp Connector\n")
+    if answer_20.lower() == "c":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-1.5
+    right=g
+    print("total no. of right answers:",right)
+    score = float(x / 100) * 100
+    print(x,"out of 100,\nthat is",score, "%")
+    if score<=100/3:
+        print('😯Oops!!!\nyou failed\nBetter luck next time......')
+    elif score<=90:
+        print('🥳congratulations!\nscored great')
+    elif score<=70:
+          print("👌\nscored average")
+    elif score<=50:
+          print("🤷‍♂\ndisappointing scores")
+    else:
+        print("😊\ncongratulations!\nperfect")
