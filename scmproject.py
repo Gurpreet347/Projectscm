595c305fc1c9c8bed5b2952c4c78ed484ad5b02f
guri <gurindersinghforever4ss@gmail.com>
2023-03-26 14:09:36 +0530

    question added

--- scmproject.py
+++ scmproject.py
@@ -151,8 +151,23 @@ if(out == '0'):
         g=g+1
     else:
         x=x-1.5
+    
     right=g
-    print("total no. of right answers:",right)
+    print("total no. of right print("22)Which state launched the ‘Mukhyamantri Vriksh Sampada Yojana’?")
+    answer_22 = input("a) odisha\nb) chattisgarh\nc) west bengal\nd) gujrat\ne)skip\n")
+    if answer_22.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-2.5
+    print("23)Which state is the key state partner of the International SME Convention 2023 (ISC)?")
+    answer_23 = input("a) odisha\nb) MP\nc) west bengal\nd) gujrat\ne)skip\n")
+    if answer_23.lower() == "b":
+        x = x + 5
+        g=g+1
+    else:
+        x=x-2.5
+ answers:",right)
     score = float(x / 100) * 100
     print(x,"out of 100,\nthat is",score, "%")
     if score<=100/3:
